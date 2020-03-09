64bd643159a4aca1c10cc69251951c01e5f4bac8
atanu231 <atanu.baidya@gmail.com>
2020-03-10 01:40:08 +0530

    Added Eureka Server and eureka client , Loadbalancing by ribbon

--- currencyconversion/src/main/java/com/atanu/currencyconversion/CurrencyconversionApplication.java
+++ currencyconversion/src/main/java/com/atanu/currencyconversion/CurrencyconversionApplication.java
@@ -2,10 +2,12 @@ package com.atanu.currencyconversion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class CurrencyconversionApplication {
 
 	public static void main(String[] args) {
---
+++ eurekanamingserver/src/test/java/com/atanu/microservice/eurekanamingserver/EurekanamingserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atanu.microservice.eurekanamingserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekanamingserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
