f6c0a84ffea8b1e354d197d24855be27dc8051de
atanu231 <atanu.baidya@gmail.com>
2020-03-10 00:29:36 +0530

    Enable Riboon client with list of servers

--- currencyconversion/src/main/java/com/atanu/currencyconversion/service/CurrencyConversionFeignProxy.java
+++ currencyconversion/src/main/java/com/atanu/currencyconversion/service/CurrencyConversionFeignProxy.java
@@ -1,12 +1,14 @@
 package com.atanu.currencyconversion.service;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.atanu.currencyconversion.dto.CurrencyConversionDTO;
 
-@FeignClient(name = "forex-service",url = "localhost:8000")
+@FeignClient(name = "forex-service")
+@RibbonClient(name="forex-service")
 public interface CurrencyConversionFeignProxy {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
